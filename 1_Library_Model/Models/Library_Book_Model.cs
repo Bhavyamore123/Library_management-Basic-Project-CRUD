fc982c49ff143051f74945b14773758dc9c4987e
Bhavesh More <99899984+Bhavyamore123@users.noreply.github.com>
2025-08-29 17:20:25 +0530

    Model

---
+++ 1_Library_Model/Models/Library_Book_Model.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Library_Model.Models
+{
+    public class Library_Book_Model
+    {
+        public int BookId { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required.")]
+        [StringLength(100, ErrorMessage = "Author name cannot exceed 100 characters.")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "ISBN is required.")]
+        [StringLength(50, ErrorMessage = "ISBN cannot exceed 50 characters.")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "Publish date is required.")]
+        [DataType(DataType.DateTime)]
+        public DateTime? PublishDate { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
+        [StringLength(50, ErrorMessage = "Category cannot exceed 50 characters.")]
+        public string Category { get; set; }
+    }
+}
+
+
+
+//Frontend(View) → Razor Views, Bootstrap, jQuery (validation, UI)
+
+//Backend (Controller) → ASP.NET MVC (C#)
+
+//Data Access Layer → Repository Pattern using ADO.NET (SqlCommand, SqlDataAdapter, DataTable)
+
+//Database → SQL Server (with your Login and Library_Book tables)
+
+//Session Management → ASP.NET Session to store authenticated user data
---
+++ 1_Library_Model/Models/Login_Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Library_Model.Models
+{
+    public class Login_Model
+    {
+        public int Login_Id { get; set; }
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Login_Email { get; set; }
+        [Required(ErrorMessage = "Please enter your password.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string Login_Password { get; set; }
+        public string Login_Username { get; set; }
+
+    }
+}

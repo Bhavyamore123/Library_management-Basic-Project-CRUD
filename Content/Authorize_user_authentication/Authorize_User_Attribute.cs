b3a8038f835a245201eb715a7cb9a4911ba4dc67
Bhavesh More <99899984+Bhavyamore123@users.noreply.github.com>
2025-08-29 17:31:14 +0530

    Library_Management/Content

---
+++ Content/Authorize_user_authentication/Authorize_User_Attribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library_Management.Authorize_user_authentication
+{
+    public class Authorize_User_Attribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = filterContext.HttpContext.Session;
+            if (session["Session_Email_ID"] == null)
+            {
+                // redirect if not logged in
+                filterContext.Result = new RedirectResult("~/Login_/LoginView");
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}

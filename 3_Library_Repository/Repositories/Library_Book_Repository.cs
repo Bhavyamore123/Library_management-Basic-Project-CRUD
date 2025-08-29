0b19e78f2e1ecaa3f254eb999aff300f60a05731
Bhavesh More <99899984+Bhavyamore123@users.noreply.github.com>
2025-08-29 17:22:34 +0530

    Repository

---
+++ 3_Library_Repository/Repositories/Library_Book_Repository.cs
@@ -0,0 +1,102 @@
+﻿using _1_Library_Model.Models;
+using _2_Library_Interface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Library_Repository.Repositories
+{
+    public class Library_Book_Repository : Library_Book_Interface
+    {
+       public SqlConnection Connect()
+       {
+            var constr = "data source=BHAVESH_MORE\\SQLEXPRESS; initial catalog=Library_Management_DB; user id=sa; password=Game@123; trustservercertificate=True;";
+            SqlConnection con = new SqlConnection(constr);
+            con.Close();
+            con.Open();
+            
+            return con;
+       }
+
+       public int PostData(Library_Book_Model model)
+        {
+            SqlConnection con = Connect();
+            SqlCommand cmd = new SqlCommand();
+            int serverresponce = 0;
+            try
+            {
+                cmd.Connection = con;
+                cmd.CommandText = "Sp_Book_Management";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                if (model.BookId == 0)
+                {
+                    cmd.Parameters.AddWithValue("@flag", 'I');
+                    serverresponce = 1;
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@flag", 'U');
+                    serverresponce = 2;
+                }
+                cmd.Parameters.AddWithValue("@BookId", model.BookId);
+                cmd.Parameters.AddWithValue("@Title", model.Title);
+                cmd.Parameters.AddWithValue("@Author", model.Author);
+                cmd.Parameters.AddWithValue("@ISBN", model.ISBN);
+                cmd.Parameters.AddWithValue("@PublishDate", model.PublishDate);
+                cmd.Parameters.AddWithValue("@Category", model.Category);
+                
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                serverresponce = 3;
+            }
+            finally
+            {
+                con.Close();
+                cmd.Dispose();
+                
+            }
+            return serverresponce;
+
+       }
+        public DataTable GetLibraryData()
+        {
+            string Constr = "select * from Book";
+            SqlCommand cmd = new SqlCommand(Constr, Connect());
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+       
+
+        public DataTable LibraryBookViewEdit(int ID)
+        {
+            string Constr = "SELECT * from Book where BookId=" +ID;
+            SqlCommand cmd = new SqlCommand(Constr, Connect());
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+           
+        }
+        public void LibraryBookViewDelete(int ID)
+        {
+            string Constr = "Delete from Book where BookId=" + ID;
+            SqlCommand cmd = new SqlCommand(Constr, Connect());
+
+            cmd.ExecuteNonQuery();
+        }
+
+
+
+
+       
+    }
+}
---
+++ 3_Library_Repository/Repositories/Login_Repository.cs
@@ -0,0 +1,39 @@
+﻿using _1_Library_Model.Models;
+using _2_Library_Interface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Library_Repository.Repositories
+{
+    public class Login_Repository : Login_Interface
+    {
+        public SqlConnection Connect()
+        {
+            string constr = "data source=BHAVESH_MORE\\SQLEXPRESS; initial catalog=Library_Management_DB; user id=sa; password=Game@123; trustservercertificate=True;";
+            SqlConnection con = new SqlConnection(constr);
+            con.Close();
+            con.Open();
+            return con;
+        }
+        public DataTable GetLoginData(Login_Model model)
+        {
+            using (SqlConnection con = new SqlConnection("data source=BHAVESH_MORE\\SQLEXPRESS; initial catalog=Library_Management_DB; user id=sa; password=Game@123; trustservercertificate=True;")) // 4
+            {
+                SqlCommand cmd = new SqlCommand("Login_SP", con); // 5 "Login_sp"
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Login_Email", model.Login_Email);
+                cmd.Parameters.AddWithValue("@Login_Password", model.Login_Password);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+    }
+}

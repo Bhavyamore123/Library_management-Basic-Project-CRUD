b3ef6acb56a9d0cd03b5140e042a4e03b2ec1972
Bhavesh More <99899984+Bhavyamore123@users.noreply.github.com>
2025-08-29 17:21:53 +0530

    Interface

---
+++ 2_Library_Interface/Interfaces/Library_Book_Interface.cs
@@ -0,0 +1,18 @@
+﻿using _1_Library_Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Library_Interface.Interfaces
+{
+    public interface Library_Book_Interface
+    {
+        int PostData(Library_Book_Model model);
+        DataTable GetLibraryData();
+        DataTable LibraryBookViewEdit(int ID);
+        void LibraryBookViewDelete(int ID);
+    }
+}
---
+++ 2_Library_Interface/Interfaces/Login_Interface.cs
@@ -0,0 +1,15 @@
+﻿using _1_Library_Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Library_Interface.Interfaces
+{
+    public interface Login_Interface
+    {
+        DataTable GetLoginData(Login_Model model);
+    }
+}
